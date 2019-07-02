01056880104b99830f4b558f6ccd719da786fb8a
Nicholas Musolino <n.musolino@gmail.com>
2019-07-02 18:42:34 -0400

    Enhance Python tests

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,26 +1,34 @@
-import contextlib
 import json
-import os
 import pathlib
 import shutil
 import subprocess
-from typing import Optional
 
 import cookiecutter.main
 import pytest
 
 
+
 def has_build_tools() -> bool:
     return all(shutil.which(program) is not None for program in ['cmake', 'make'])
+
 
 def template_directory() -> pathlib.Path:
     """ Return the root directory containing cookiecutter.json. """
     return pathlib.Path(__file__).parent.parent
 
 
-def template_context() -> dict:
+def parameterize_by_template_parameter(parameter_name):
+    template = _template()
+    parameter_choices = list(template[parameter_name])
+
+    return pytest.mark.parametrize(parameter_name,
+                                   [pytest.param(value, id='{}={!r}'.format(parameter_name, value)) for value in
+                                    parameter_choices])
+
+def _template() -> dict:
     with open(template_directory() / "cookiecutter.json") as f:
         return json.load(f)
+
 
 
 @pytest.fixture()
@@ -29,7 +37,7 @@ def cookiecutter_renderer(tmp_path: pathlib.Path):
     output_dir = (tmp_path / "output")
     output_dir.mkdir()
 
-    def render_template(extra_context: Optional[dict] = None) -> pathlib.Path:
+    def render_template(**extra_context) -> pathlib.Path:
         """ Return the result of running cookiecutter with the given context. """
         return pathlib.Path(
             cookiecutter.main.cookiecutter(template=template_directory().as_posix(),
@@ -37,17 +45,8 @@ def cookiecutter_renderer(tmp_path: pathlib.Path):
                                            extra_context=extra_context,
                                            output_dir=output_dir)
         )
+
     return render_template
-
-
-# @contextlib.contextmanager
-# def chdir(directory: pathlib.Path):
-#     prev_cwd = pathlib.Path.cwd()
-#     os.chdir(directory)
-#     try:
-#         yield
-#     finally:
-#         os.chdir(prev_cwd)
 
 
 # Unit tests
@@ -55,14 +54,15 @@ def test_template(cookiecutter_renderer):
     cookiecutter_renderer()
 
 
-@pytest.mark.parametrize('license', template_context()['license'])
+@parameterize_by_template_parameter('license')
 def test_license_choices(license, cookiecutter_renderer):
     cookiecutter_renderer()
 
 
 @pytest.mark.skipif(not has_build_tools(), reason='Does not have cmake and make')
-def test_build(cookiecutter_renderer):
-    project_path = cookiecutter_renderer()
+@parameterize_by_template_parameter('use_boost')
+def test_build(cookiecutter_renderer, use_boost):
+    project_path = cookiecutter_renderer(use_boost=use_boost)
     build_dir = (project_path / "build")
     build_dir.mkdir()
     cmake_result = subprocess.run(['cmake', '..'], cwd=build_dir, check=True)
