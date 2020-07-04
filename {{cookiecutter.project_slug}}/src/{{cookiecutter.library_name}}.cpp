c8cb9d91b64c8ec04510f31b3f79e9b76ca179d1
Michael Wagner <michael.wagner.31337@gmail.com>
2020-07-04 14:09:46 +0200

    Add target to create docs with doxygen

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.library_name}}.cpp
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.library_name}}.cpp
@@ -1,7 +1,32 @@
+/**
+ **************************************************
+ * @file     {{cookiecutter.library_name}}.cpp
+ * @author   {{cookiecutter.full_name}}
+ * @date     {% now 'local', '%Y-%m-%d' %}
+ * @brief Some brief description about this module
+ *
+ */
+
+/** @mainpage {{cookiecutter.project_name}}
+ *
+ * This is a cool project that does such and such.
+ *
+ */
+
 #include <{{cookiecutter.library_name}}/{{cookiecutter.library_name}}.hpp>
 
+/*! @brief about {{cookiecutter.cpp_namespace}}
+*
+*  some bla about {{cookiecutter.cpp_namespace}}
+*/
 namespace {{cookiecutter.cpp_namespace}} {
 
+    /*! @brief about message()
+    *
+    *  some bla about message()
+    * @param recipient The thing or person that shall be greeted.
+    * @return message
+    */
     std::string message(const std::string& recipient)
     {
         const std::string greeting { "Hello" };
