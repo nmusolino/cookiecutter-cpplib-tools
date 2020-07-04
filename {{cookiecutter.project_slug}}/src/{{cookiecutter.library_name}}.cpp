3593705090eabc37b5a98a49ebb349f7f2060048
Michael Wagner <michael.wagner.31337@gmail.com>
2020-07-04 21:30:06 +0200

    Move doxygen target to CMakeLists.txt

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.library_name}}.cpp
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.library_name}}.cpp
@@ -3,7 +3,9 @@
  * @file     {{cookiecutter.library_name}}.cpp
  * @author   {{cookiecutter.full_name}}
  * @date     {% now 'local', '%Y-%m-%d' %}
- * @brief Some brief description about this module
+ * @brief Some brief description about this module.
+ * @details
+ * Some long description about this module.
  *
  */
 
@@ -15,18 +17,19 @@
 
 #include <{{cookiecutter.library_name}}/{{cookiecutter.library_name}}.hpp>
 
-/*! @brief about {{cookiecutter.cpp_namespace}}
-*
-*  some bla about {{cookiecutter.cpp_namespace}}
-*/
+/*! @brief Some brief bla about {{cookiecutter.cpp_namespace}}.
+ *	@details
+ *  Some long bla about {{cookiecutter.cpp_namespace}}.
+ */
 namespace {{cookiecutter.cpp_namespace}} {
 
-    /*! @brief about message()
-    *
-    *  some bla about message()
-    * @param recipient The thing or person that shall be greeted.
-    * @return message
-    */
+    /*!	@brief Some brief bla about message().
+     *	@details
+     *  Some long bla about message().
+     *
+     * 	@param recipient The thing or person that shall be greeted.
+     * 	@return message
+     */
     std::string message(const std::string& recipient)
     {
         const std::string greeting { "Hello" };
